b204c9c841f3a966006d92f9514dee4ef22d845e
Skonor <gorbunovmikh73@gmail.com>
2023-10-14 00:40:06 +0300

    init bug fix in models

--- hw_asr/model/__init__.py
+++ hw_asr/model/__init__.py
@@ -1,9 +1,9 @@
 from hw_asr.model.baseline_model import BaselineModel
-from hw_asr.model.deep_speech2_no_bn import DeepSpeech2NoBN
 from hw_asr.model.deep_speech2 import DeepSpeech2
+from hw_asr.model.deep_speech2_no_bn import DeepSpeech2NoBN
 
 __all__ = [
     "BaselineModel",
     "DeepSpeech2",
-    "DeepSpeech2BN"
+    "DeepSpeech2NoBN"
 ]
