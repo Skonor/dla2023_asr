ff2300e63fa0bd23762bce58959922f40dd3fe58
Skonor <gorbunovmikh73@gmail.com>
2023-10-17 22:22:52 +0300

    kaggle fix i hate kaggle

--- hw_asr/datasets/librispeech_dataset_kaggle.py
+++ hw_asr/datasets/librispeech_dataset_kaggle.py
@@ -69,29 +69,31 @@ class LibrispeechDatasetKaggle(BaseDataset):
 
     def _create_index(self, part):
         index = []
+        trans_dir = self._data_dir / 'meta' / part
         split_dir = self._data_dir / part
+        
         if not split_dir.exists():
             self._load_part(part)
 
-        flac_dirs = set()
-        for dirpath, dirnames, filenames in os.walk(str(split_dir)):
-            if any([f.endswith(".flac") for f in filenames]):
-                flac_dirs.add(dirpath)
-        for flac_dir in tqdm(
-                list(flac_dirs), desc=f"Preparing librispeech folders: {part}"
+        trans_dirs = set()
+        for dirpath, dirnames, filenames in os.walk(str(trans_dir)):
+            if any([f.endswith(".txt") for f in filenames]):
+                trans_dirs.add(dirpath)
+        for trans_dir in tqdm(
+                list(trans_dirs), desc=f"Preparing librispeech folders: {part}"
         ):
-            flac_dir = Path(flac_dir)
-            trans_path = list(flac_dir.glob("*.trans.txt"))[0]
+            trans_dir = Path(trans_dir)
+            trans_path = list(trans_dir.glob("*.trans.txt"))[0]
             with trans_path.open() as f:
                 for line in f:
                     f_id = line.split()[0]
                     f_text = " ".join(line.split()[1:]).strip()
-                    flac_path = flac_dir / f"{f_id}.flac"
-                    t_info = torchaudio.info(str(flac_path))
+                    wav_path = split_dir / f"{f_id}.wav"
+                    t_info = torchaudio.info(str(wav_path))
                     length = t_info.num_frames / t_info.sample_rate
                     index.append(
                         {
-                            "path": str(flac_path.absolute().resolve()),
+                            "path": str(wav_path.absolute().resolve()),
                             "text": f_text.lower(),
                             "audio_len": length,
                         }
