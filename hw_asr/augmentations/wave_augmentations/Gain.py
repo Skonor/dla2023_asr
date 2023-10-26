5a50b6dece5dca18f67f00828fa01a3dfbfbfdcc
Skonor <gorbunovmikh73@gmail.com>
2023-10-26 16:19:35 +0300

    add random gain aug

--- hw_asr/augmentations/wave_augmentations/Gain.py
+++ hw_asr/augmentations/wave_augmentations/Gain.py
@@ -2,6 +2,7 @@ import torch_audiomentations
 from torch import Tensor
 
 from hw_asr.augmentations.base import AugmentationBase
+from hw_asr.augmentations.random_apply import RandomApply
 
 
 class Gain(AugmentationBase):
@@ -11,3 +12,13 @@ class Gain(AugmentationBase):
     def __call__(self, data: Tensor):
         x = data.unsqueeze(1)
         return self._aug(x).squeeze(1)
+
+
+class RandomGain(RandomApply):
+    def __init__(self, *args, **kwargs):
+        if 'p' not in kwargs:
+            p = 0.3
+        else:
+            p = kwargs['p']
+            del kwargs['p']
+        super.__init__(Gain(*args, **kwargs), p = p)
