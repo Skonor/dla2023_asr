4e6a14b292b52047f83e35b7bdbf048009c6efc4
Skonor <gorbunovmikh73@gmail.com>
2023-10-26 22:32:51 +0300

    delete beam search from test with metrics

--- test_with_metrics.py
+++ test_with_metrics.py
@@ -85,10 +85,7 @@ def main(config, out_file):
                 results.append(
                     {
                         "ground_trurh": batch["text"][i],
-                        "pred_text_argmax": text_encoder.ctc_decode(argmax.cpu().numpy()),
-                        "pred_text_beam_search": text_encoder.ctc_beam_search(
-                            batch["probs"][i], batch["log_probs_length"][i], beam_size=100
-                        )[:10],
+                        "pred_text_argmax": text_encoder.ctc_decode(argmax.cpu().numpy())
                     }
                 )
 
