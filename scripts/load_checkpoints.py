8c0afd33168a9ccbcf25860faeff14b2b86eb445
Skonor <gorbunovmikh73@gmail.com>
2023-10-27 13:58:59 +0300

    fix script

--- scripts/load_checkpoints.py
+++ scripts/load_checkpoints.py
@@ -11,13 +11,13 @@ URL_LINKS = {
 def main():
     dir = Path(__file__).absolute().resolve().parent.parent
     for name in URL_LINKS:
-        checkpoint_dir = dir / 'saved' / 'models' / 'checkpoints' / URL_LINKS[name]
-        checkpoint_dir.mkdir(exists_ok=True, parents=True)
-        zip_pth = checkpoint_dir / (URL_LINKS[name] + '.zip')
+        checkpoint_dir = dir / 'saved' / 'models' / 'checkpoints' / name
+        checkpoint_dir.mkdir(exist_ok=True, parents=True)
+        zip_pth = checkpoint_dir / (name + '.zip')
         model_pth = checkpoint_dir / 'model_weights.pth'
         if not model_pth.exists():
-            gdown.download(URL_LINKS[name], zip_pth, quiet=False)
-            shutil.unpack_archive(zip_pth, checkpoint_dir, "zip")
+            gdown.download(URL_LINKS[name], str(zip_pth), quiet=False)
+            shutil.unpack_archive(str(zip_pth), str(checkpoint_dir), "zip")
             os.remove(zip_pth)
 
 if __name__ == "__main__":
