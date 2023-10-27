86af6bdf33037857a12e527dda169cdf81805537
Skonor <gorbunovmikh73@gmail.com>
2023-10-27 13:34:25 +0300

    change link

--- scripts/load_checkpoints.py
+++ scripts/load_checkpoints.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 
 URL_LINKS: {
-    'DeepSpeech2_clean': 'https://drive.google.com/uc?id=1_FP6Rl52nOb_ylx345UW_HCfWl4sYhcl'
+    'DeepSpeech2_clean': 'https://drive.google.com/uc?id=16N0g78RAdXeJkRFmPwzk2BstvMA1jB0A'
 }
 
 def main():
@@ -16,7 +16,7 @@ def main():
         zip_pth = checkpoint_dir / (URL_LINKS[name] + '.zip')
         model_pth = checkpoint_dir / 'model_weights.pth'
         if not model_pth.exists():
-            gdown.download(URL_LINKS[name], zip_pth)
+            gdown.download(URL_LINKS[name], zip_pth, quiet=False)
             shuitl.unpack_archive(zip_pth, checkpoint_dir, "zip")
             os.remove(zip_pth)
 
