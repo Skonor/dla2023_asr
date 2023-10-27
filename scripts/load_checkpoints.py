7372227641dba1e5dabefc87bf8f1b064d369438
Skonor <gorbunovmikh73@gmail.com>
2023-10-27 13:25:32 +0300

    add checkpoint loading script and config

--- scripts/load_checkpoints.py
+++ scripts/load_checkpoints.py
@@ -1,9 +1,24 @@
+import gdown
+import shuitl
+import os
 from pathlib import Path
 
 
 URL_LINKS: {
-    'DeepSpeech2_clean80epochs': '...'
+    'DeepSpeech2_clean': 'https://drive.google.com/uc?id=1_FP6Rl52nOb_ylx345UW_HCfWl4sYhcl'
 }
 
 def main():
-    pass
+    dir = Path(__file__).absolute().resolve().parent.parent
+    for name in URL_LINKS:
+        checkpoint_dir = dir / 'saved' / 'models' / 'checkpoints' / URL_LINKS[name]
+        checkpoint_dir.mkdir(exists_ok=True, parents=True)
+        zip_pth = checkpoint_dir / (URL_LINKS[name] + '.zip')
+        model_pth = checkpoint_dir / 'model_weights.pth'
+        if not model_pth.exists():
+            gdown.download(URL_LINKS[name], zip_pth)
+            shuitl.unpack_archive(zip_pth, checkpoint_dir, "zip")
+            os.remove(zip_pth)
+
+if __name__ == "__main__":
+    main()
